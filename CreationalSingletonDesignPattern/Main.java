23edacf59e7f788428d13d972a670b727cf7f279
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-09-19 12:21:10 +0530

    Singleton usecase scenario for logging service written implementation example

---
+++ CreationalSingletonDesignPattern/Main.java
@@ -0,0 +1,13 @@
+package CreationalSingletonDesignPattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        logger1.log("Application started.");
+        logger1.log("Processing data...");
+
+        Logger logger2 = Logger.getInstance();
+        System.out.println("Are both logger instances the same? " + (logger1 == logger2));
+        logger2.log("Application ended.");
+    }
+}
